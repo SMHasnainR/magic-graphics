03341d8a7b88db05dea8c954a03d48a18641b6bc
Hasnain <hasnain.shoaib@naheed.pk>
2023-09-26 22:51:17 +0500

    packages changes

--- config/constants.php
+++ config/constants.php
@@ -1,28 +1,25 @@
-<!-- 
-    // Here are the all the constants that will be used globbaly throughout the project
--->
 <?php
-
+// Here are the all the constants that will be used globbaly throughout the project
 
 return [
     "packages" => [
         [
             'id' => 1,
-            'name' => 'ZERO PACKAGE',
-            'price' => 100,
+            'name' => 'Pay By Design',
+            'price' => '-',
             'features' => [
-                'limited to 30 creatives per month',
-                '1 brand',
+                'Custom',
             ]
         ],
         [
             'id' => 2,
-            'name' => 'One PACKAGE',
+            'name' => 'Standard PACKAGE',
             'price' => 200,
             'features' => [
                 'limited to 30 creatives per month',
                 '1 dedicated graphic designer',
-                '10 brand',
+                '2 Revisions',
+                '1 brand',
             ]
         ],
         [
@@ -32,8 +29,9 @@ return [
             'features' => [
                 'limited to 30 creatives per month',
                 '1 dedicated graphic designer',
-                '20 revisions',
-                '1 brand',
+                '5 Vidoes upto 1 min each',
+                '3 revisions',
+                '2 brand',
             ],
         ],
         [
@@ -41,32 +39,32 @@ return [
             'name' => 'Gold Package',
             'price' => 400,
             'features' => [
-                'limited to 30 creatives per month',
+                'Upto to 90 creatives per month',
                 '100 dedicated graphic designer',
-                '2 revisions',
-                '1 brand',
+                '5 Vidoes upto 30 sec each',
+                '5 revisions',
+                '3 brand',
             ],
         ],
         [
             'id' => 5,
             'name' => 'Corporate Package',
-            'price' => 200,
+            'price' => 850,
             'features' => [
-                'limited to 30 creatives per month',
-                '1 dedicated graphic designer',
-                '2 revisions',
-                '12 brand',
+                'Upto to 120 creatives per month',
+                '2 dedicated graphic designer',
+                '2 dedicated motion/graphic designer',
+                '10 videos upto 1 min each',
+                'Unlimited Revisions',
+                '4 brand',
             ],
         ],
         [
             'id' => 6,
             'name' => 'Elite Package',
-            'price' => 200,
+            'price' => '-',
             'features' => [
-                'limited to 30 creatives per month',
-                '1 dedicated graphic designer',
-                '2 revisions',
-                '1 brand',
+                'Call for details',
             ],
         ],
     ]
